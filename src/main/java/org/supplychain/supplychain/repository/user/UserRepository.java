6a59d315750f31d10e33f2abc10135c1a9371b24
mustapha <mustaphaamoutaki@gmail.com>
2025-12-17 08:53:44 +0100

    feat: find user by email and username

---
+++ src/main/java/org/supplychain/supplychain/repository/user/UserRepository.java
@@ -0,0 +1,13 @@
+package org.supplychain.supplychain.repository.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.supplychain.supplychain.model.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
