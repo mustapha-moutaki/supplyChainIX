e48156329c27b960bce5540c241f431fc28bcf99
mustapha <mustaphaamoutaki@gmail.com>
2025-12-17 08:56:43 +0100

    feat: create refreshToken model

---
+++ src/main/java/org/supplychain/supplychain/model/RefreshToken.java
@@ -0,0 +1,61 @@
+package org.supplychain.supplychain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class RefreshToken {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+public Long getId() {
+    return id;
+}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Instant getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Instant expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
